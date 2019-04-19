87dc759482fde79b7b6ba76465d65e16018802fe
eliemugenzi <eliemugenzi@gmail.com>
2019-04-19 05:17:55 +0200

    Add builded files

---
+++ build/precache-manifest.85b1d9f3005f93e4c377aeaf01e1b167.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4cef41a70d3133a0435f",
+    "url": "/static/js/main.d3fd8972.chunk.js"
+  },
+  {
+    "revision": "b47b4ffedb9a93c50705",
+    "url": "/static/js/2.117b9808.chunk.js"
+  },
+  {
+    "revision": "4cef41a70d3133a0435f",
+    "url": "/static/css/main.a3617bf3.chunk.css"
+  },
+  {
+    "revision": "4fb2052745d1e68e543b59ac28db2ee9",
+    "url": "/index.html"
+  }
+];
