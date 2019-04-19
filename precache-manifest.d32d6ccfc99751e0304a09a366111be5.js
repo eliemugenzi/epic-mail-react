abfdb18d5b8306e77fd46dc8e5c15534165b5606
eliemugenzi <eliemugenzi@gmail.com>
2019-04-19 05:19:13 +0200

    Updates

---
+++ precache-manifest.d32d6ccfc99751e0304a09a366111be5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb09fca89fe9bc25de86",
+    "url": "/epic-mail-react/static/js/runtime~main.7879833d.js"
+  },
+  {
+    "revision": "f6452457cf6cfb773aa5",
+    "url": "/epic-mail-react/static/js/main.5287169a.chunk.js"
+  },
+  {
+    "revision": "b47b4ffedb9a93c50705",
+    "url": "/epic-mail-react/static/js/2.117b9808.chunk.js"
+  },
+  {
+    "revision": "f6452457cf6cfb773aa5",
+    "url": "/epic-mail-react/static/css/main.a3617bf3.chunk.css"
+  },
+  {
+    "revision": "98a9d269468688051c8ee5650c6ecca3",
+    "url": "/epic-mail-react/index.html"
+  }
+];
